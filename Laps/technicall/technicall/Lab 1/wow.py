29fcd60ffb119314ebaf98ee43aecaedab34b270
iZDX7 <tlazyad91@gmail.com>
2026-01-25 13:19:14 +0300

    edited

--- Laps/technicall/technicall/Lab 1/wow.py
+++
@@ -1,9 +0,0 @@
-from functions import mult, add, subtract, divide, power, modulus, floor_divide
-
-print("Multiplication:", mult(1, 2, 3, 4))
-print("Addition:", add(5, 3))
-print("Subtraction:", subtract(10, 4))
-print("Division:", divide(20, 4))
-print("Power:", power(2, 3))
-print("Modulus:", modulus(10, 3))
-print("Floor Division:", floor_divide(20, 3)) 
